837e46e4909466a13b29b54e9c0556cb0bb90700
von <charles@mogoroom.com>
2020-07-20 22:30:36 +0800

    xxx

--- mytoy-web/src/main/java/cc/fyp/toy/init/Application.java
+++ mytoy-web/src/main/java/cc/fyp/toy/init/Application.java
@@ -9,9 +9,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("cc.fyp")
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application  extends SpringBootServletInitializer {
 
     @Override
